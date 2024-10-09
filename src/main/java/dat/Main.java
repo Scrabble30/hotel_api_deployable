7997bf99c4d6dadeca56f254b518a681c8543264
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-10-09 10:50:53 +0200

    Add log message

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -6,6 +6,8 @@ import dat.config.Populate;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Starting server...");
+
         ApplicationConfig.startServer(7070);
         Populate.main(args);
     }
