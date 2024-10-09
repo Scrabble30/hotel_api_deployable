3711006ba8e648dc336ef7ab979359e131721383
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-10-09 11:53:27 +0200

    Update system out println message

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -6,7 +6,7 @@ import dat.config.Populate;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Starting server...");
+        System.out.println("Starting the server...");
 
         ApplicationConfig.startServer(7070);
         Populate.main(args);
