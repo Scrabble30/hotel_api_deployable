486accfaf06945a24950f9b9479fcf2449232c99
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-10-09 10:11:02 +0200

    Populate with data

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -1,18 +1,12 @@
 package dat;
 
 import dat.config.ApplicationConfig;
-import dat.controllers.impl.ExceptionController;
-import dat.exceptions.ApiException;
-import dat.exceptions.Message;
-import dat.routes.Routes;
-import io.javalin.Javalin;
-import io.javalin.http.Context;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import dat.config.Populate;
 
 public class Main {
 
     public static void main(String[] args) {
         ApplicationConfig.startServer(7070);
+        Populate.main(args);
     }
 }
